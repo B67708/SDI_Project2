f5026d4792b199d43870de7eb3c51526bae2dea0
John Cardiff-Winchell <johnwinchell86@fullsail.edu>
2013-02-14 17:06:12 -0500

    Template filled out, beginning variables created.

--- CardiffWinchell_John_assignment2.js
+++ CardiffWinchell_John_assignment2.js
@@ -1,1 +1,19 @@
-alert("JavaScript works!");
+// Author John Cardiff-Winchell
+// Date 2/14/2013
+// Class Scalable Data Infastructures
+//Project 2 -  Picking a place to go out and eat.
+
+// Variables
+
+var fastFood = 1,
+	sitDownResturaunt = 2;
+	
+var fastFoodArray = ["Mcdonald's", "Taco Bell", "Burger King", "Wendy's", "Rally's"];
+
+var sitDownResturauntArray = ["Pizza Hut", "Applebee's", "Tony's", "Don Pablo's", "Bennigan's",];
+
+var question1 = "Do you want to go out for dinner tonight?",
+	question2 = "Are you in the mood for fast food or a sit down resturaunt?";
+	
+
+
